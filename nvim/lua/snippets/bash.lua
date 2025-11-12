184a5ef995c3b9058ad25677deb5dc96af3e71c6
zharkaron <daron.valdiviaceron@gmail.com>
2025-11-12 00:40:25 -0600

    added new stuff now

--- nvim/lua/snippets/bash.lua
+++ nvim/lua/snippets/bash.lua
@@ -4,44 +4,49 @@ local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
 
+-- Helper to register the same snippets for both 'sh' and 'bash'
+local filetypes = { "sh", "bash" }
 
--- Boilerplate snippet for new Bash files
-ls.add_snippets("sh", {
-    s("newfile", {  -- trigger word, can be anything
-        t({"#!/bin/bash", -- the first line
-        "# Author: Zharkaron",
-        "# Description: "}),
-        i(1, "Description here"),
-        t({""}),
-        i(0)
-    }),
-})
+local bash_snips = {
+  -- Shebang boilerplate
+  s("newfile", {
+    t({"#!/usr/bin/env bash", "# Author: Zharkaron", "# Description: "}),
+    i(1, "Description here"),
+    i(0),
+  }),
 
--- Bash snippets
-ls.add_snippets("sh", {
-  s("ifblock", {
+  -- if ... then ... fi
+  s("if", {
     t("if "), i(1, "condition"), t("; then"),
     t({"", "  "}), i(2, "body"),
     t({"", "fi"}),
-    i(0)
+    i(0),
   }),
-  s("forblock", {
+
+  -- for var in list; do ... done
+  s("for", {
     t("for "), i(1, "var"), t(" in "), i(2, "list"), t("; do"),
     t({"", "  "}), i(3, "body"),
     t({"", "done"}),
-    i(0)
+    i(0),
   }),
-  s("whileblock", {
+
+  -- while ... do ... done
+  s("while", {
     t("while "), i(1, "condition"), t("; do"),
     t({"", "  "}), i(2, "body"),
     t({"", "done"}),
-    i(0)
+    i(0),
   }),
-  s("caseblock", {
+
+  -- case ... in ... esac
+  s("case", {
     t("case "), i(1, "word"), t(" in"),
-    t({"", "  "}), i(2, "pattern"), t(") "),
-    i(3, "body"), t({"", "    ;;"}),
-    t({"", "esac"}),
-    i(0)
+    t({"", "  "}), i(2, "pattern"), t(")"),
+    t({"", "  "}), i(3, "body"),
+    t({"", "    ;;", "esac"}),
+    i(0),
   }),
-})
+}
+
+ls.add_snippets(filetypes, bash_snips)
