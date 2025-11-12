6ce922de56a00db4c67d5cb682384c51a8d6ce44
zharkaron <daron.valdiviaceron@gmail.com>
2025-11-12 01:41:18 -0600

    Had to go back to the old stuff cause this shit didndt work

--- nvim/lua/snippets/config.lua
+++ nvim/lua/snippets/config.lua
@@ -1,35 +1,59 @@
 local ls = require("luasnip")
-local from_lua = require("luasnip.loaders.from_lua")
+require("snippets.bash")
+require("snippets.html")
+require("snippets.python")
 
--- Automatically load all snippets from the `snippets` folder
-from_lua.lazy_load({ paths = vim.fn.stdpath("config") .. "/lua/snippets" })
-
--- Keymaps
+-- Keymaps for expanding/jumping snippets safely
 vim.keymap.set({"i", "s"}, "<Tab>", function()
-  if ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n", true)
-  end
-end, {silent = true, expr = false})
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    else
+        -- Fallback: insert a real Tab character
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n", true)
+    end
+end, {silent = true})
 
 vim.keymap.set({"i", "s"}, "<S-Tab>", function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, true, true), "n", true)
-  end
-end, {silent = true, expr = false})
-
--- Optional: auto-expand template snippet for new files
-vim.api.nvim_create_autocmd("BufNewFile", {
-  pattern = {"*.html", "*.sh", "*.py"},
-  callback = function(event)
-    local ft = vim.bo[event.buf].filetype
-    local snippets = ls.get_snippets(ft)
-    if snippets and #snippets > 0 then
-      ls.snip_expand(snippets[1])
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, true, true), "n", true)
     end
-  end,
+end, {silent = true})
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.html",
+    callback = function()
+        local ls = require("luasnip")
+        local snippets = require("luasnip").get_snippets("html")
+        if snippets and #snippets > 0 then
+            -- expand the first snippet (your template)
+            ls.snip_expand(snippets[1])
+        end
+    end,
 })
 
+
+-- bash pre write
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.sh",
+    callback = function()
+        local ls = require("luasnip")
+        local snippet = require("luasnip").get_snippets("sh")[1]  -- first snippet, your newfile snippet
+        if snippet then
+            ls.snip_expand(snippet)
+        end
+    end,
+})
+
+-- Auto-expand for Python files
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.py",
+    callback = function()
+        local ls = require("luasnip")
+        local snippets = ls.get_snippets("python")
+        if snippets and #snippets > 0 then
+            ls.snip_expand(snippets[1])
+        end
+    end,
+})
